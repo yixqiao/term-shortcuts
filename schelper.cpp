8dd44c0351527052a14d0d8825b7f386bf0cdeb6
Yixuan Qiao <yixqiao@gmail.com>
2020-05-07 20:22:06 -0700

    Bug fix for same token
    Registering the same token will now result in an error message

--- schelper.cpp
+++ schelper.cpp
@@ -45,55 +45,55 @@ int main(int argc, char *argvc[]) {
         }
       }
     }
-  } else {
-    if (argv[1] == "-a") {
-      std::map<std::string, std::string> commands;
+  } else if (argv[1] == "-a") {
+    std::map<std::string, std::string> commands;
 
-      if (argv[2].find("-") != std::string::npos) {
-        std::cout << "Commands cannot contain the character \"-\"." << std::endl
-                  << std::endl;
-        printHelp();
-      }
-      std::string newToken = argv[2];
+    if (argv[2].find("-") != std::string::npos) {
+      std::cout << "Commands cannot contain the character \"-\"." << std::endl
+                << std::endl;
+      printHelp();
+    }
+    std::string newToken = argv[2];
 
-      // Read commands
-      std::ifstream comFile("commands.txt");
-      if (comFile.good()) {
-        while (!comFile.eof()) {
-          std::string curLine;
-          std::getline(comFile, curLine);
-          std::string token = curLine.substr(0, curLine.find("-"));
-          if (token == newToken) {
-            ecout << "This token is already taken!" << std::endl;
-            exit(0);
-          }
-        }
-      }
-
-      if (argc < 4) {
-        printHelp();
-      }
-
-      if (argv[3] == "-p") {
-        std::string histPath = std::string(getenv("HOME")) + "/.bash_history";
-
-        std::ifstream comFile(histPath);
+    // Read commands
+    std::ifstream comFile("commands.txt");
+    if (comFile.good()) {
+      while (!comFile.eof()) {
         std::string curLine;
-        std::string lastCommand;
-        if (comFile.good()) {
-          while (!comFile.eof()) {
-            lastCommand = curLine;
-            std::getline(comFile, curLine);
-            // std::cout << curLine << std::endl;
-          }
+        std::getline(comFile, curLine);
+        std::string token = curLine.substr(0, curLine.find(" "));
+        if (token == newToken) {
+          ecout << "This token is already taken!" << std::endl;
+          exit(0);
         }
-        std::ofstream commandsFile;
-        commandsFile.open("commands.txt", std::ios_base::app);
-        commandsFile << newToken << " " <<  lastCommand << std::endl;
-        ecout << "Write successful: " << newToken << ": " << lastCommand << std::endl;
       }
     }
+
+    if (argc < 4) {
+      printHelp();
+    }
+
+    if (argv[3] == "-p") {
+      std::string histPath = std::string(getenv("HOME")) + "/.bash_history";
+
+      std::ifstream comFile(histPath);
+      std::string curLine;
+      std::string lastCommand;
+      if (comFile.good()) {
+        while (!comFile.eof()) {
+          lastCommand = curLine;
+          std::getline(comFile, curLine);
+          // std::cout << curLine << std::endl;
+        }
+      }
+      std::ofstream commandsFile;
+      commandsFile.open("commands.txt", std::ios_base::app);
+      commandsFile << newToken << " " << lastCommand << std::endl;
+      ecout << "Write successful: " << newToken << ": " << lastCommand
+            << std::endl;
+    }
   }
+
   // ofstream myfile;
   // myfile.open("example.txt");
   // myfile << "Writing this to a file.\n";
